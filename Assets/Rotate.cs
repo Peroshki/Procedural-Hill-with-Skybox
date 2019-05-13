0658f5b9c425d105fb559abbe32c224adb5c7686
Alan Vasilkovsky <alan.vasilkovsky@gmail.com>
2019-05-12 23:27:35 -0700

    Add files via upload

---
+++ Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A simple script which rotates the camera around an axis
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+    }
+}
